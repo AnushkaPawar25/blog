c214a760e6e37ad78aafa50211c085ddcce6c513
Anushka <anushkakpawar25@gmail.com>
2024-11-03 01:11:23 +0530

    remove node_modules and add to .gitignore

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const { default: mongoose } = require('mongoose')
 const Blog  = require('../models/Blog')
 
 blogRouter.get('/', async (req, res, next) =>{
@@ -37,6 +38,37 @@ blogRouter.post('/', async(req, res, next) =>{
     }
 })
 
+blogRouter.get('/:id', async(req, res, next) =>{
+    // console.log("in id part")
+    try{
+        // console.log('in try block of id part')
+        const blogId = req.params.id;
+        const blog = await Blog.findById(blogId)
 
+        if (blog){
+            res.json(blog);
+        } else{
+            res.status(404).send('Blog not found')
+        }
+    }catch(error) {
+        next(error)
+    }
+})
+
+blogRouter.delete('/:id', async(req, res, next) =>{
+    try{
+        const blogId = req.params.id
+
+        const deletedBlog = await Blog.findByIdAndDelete(blogId)
+
+        if (deletedBlog) {
+            res.status(204).send()
+        }else{
+            res.status(404).send("Blog not found")
+        }
+    }catch(error){
+        next(error)
+    }
+})
 module.exports = blogRouter
 
--- models/Blog.js
+++ models/Blog.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    content: String, 
-    likeCount: {type: Number, default: 0}, 
-    viewCount: {type: Number, default: 0},
-}, {timestamps : true})
+    title: { type: String, required: true }, 
+    author: { type: String, required: true }, 
+    content: { type: String, required: true }, 
+    likeCount: { type: Number, default: 0 },
+    viewCount: { type: Number, default: 0 },
+}, { timestamps: true });
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
     }
-})
-// ljfladjf
+});
 
-module.exports = mongoose.model('Blog',blogSchema )
+module.exports = mongoose.model('Blog', blogSchema);
